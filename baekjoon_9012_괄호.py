83c404fddd26e07b07e9d6d2a7e063fbee7576c5
구선민 <fhdahd899@gmail.com>
2021-01-09 11:59:56 +0900

    20210109

--- baekjoon_9012_괄호.py
+++ baekjoon_9012_괄호.py
@@ -1,1 +1,25 @@
-#잔디심기 test
+def is_vps(s):
+
+    vps = 0
+
+    for s in string:
+        if s == '(':
+            vps += 1
+        elif s == ')':
+            vps -= 1
+
+        if vps < 0:
+            return "NO"
+
+    if vps == 0:
+        return "YES"
+    else:
+        return "NO"
+
+t = int(input())
+
+for _ in range(t):
+    string = list(max(input().split()))
+    print(is_vps(string))
+
+
